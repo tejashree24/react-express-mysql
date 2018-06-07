611ef0f31e59c37e37efc77fd5483d98064b502b
tejashree24 <tejashreedhole24@gmail.com>
2018-06-07 19:59:32 +0530

    changes in server.js tablename changed

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
 const express = require('express');
+		const cors = require('cors');
+		const mysql = require('mysql');
 		const app = express();
 		const port = 5000;
 
+		const SELECT_BOOKLIST_QUERY = "SELECT * FROM books";
+
+		const connection = mysql.createConnection({
+			host:'localhost',
+			user: 'root',
+			password: 'password',
+			database: 'book_lib'
+		});
+
+		connection.connect( err =>{
+			if(err){
+				return err;
+			}
+
+		});
+
+		app.use(cors());
+
+		//SELECT QUERY
+
+		app.get('/booklist',(req,res)=>{
+			connection.query(SELECT_BOOKLIST_QUERY,(err,results)=>{
+				if(err){
+					return res.send(err)
+				}
+				else
+				{
+					return res.json({
+						data:results
+					})
+				}
+			});
+			
+		});
+
+
 		app.get('/',(req,res)=>{
-			res.send('go to /customers to see customers')
-			// res.json(customers);
+			res.send('go to /booklist to see booklist')
+			// res.json(booklist);
 		});
 
 		app.listen(port, ()=>{
