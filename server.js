34a2a419ead9ff14bf79a66139e2994f1b2bd888
tejashree24 <tejashreedhole24@gmail.com>
2018-06-18 18:34:42 +0530

    database commit

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const express = require('express');
 		const app = express();
 		const port = 5000;
 
+<<<<<<< Updated upstream
 		const SELECT_BOOKLIST_QUERY = "SELECT * FROM books";
+=======
+<<<<<<< Updated upstream
+=======
+		//SELECT QUERY FOR BOOKS
+		const SELECT_BOOKLIST_QUERY = "SELECT * FROM books";
+		const SELECT_FICTION_QUERY = "SELECT * FROM fiction";
+		const SELECT_NONFICTION_QUERY = 'SELECT * FROM nonfiction';
+>>>>>>> Stashed changes
 
 		const connection = mysql.createConnection({
 			host:'localhost',
@@ -39,7 +48,41 @@ const express = require('express');
 			
 		});
 
+<<<<<<< Updated upstream
 
+=======
+		app.get('/fiction',(req,res)=>{
+			connection.query(SELECT_FICTION_QUERY,(err,results)=>{
+				if(err){
+					return res.send(err)
+				}
+				else
+				{
+					return res.json({
+						data:results
+					})
+				}
+			});
+			
+		});
+
+		app.get('/nonfiction',(req,res)=>{
+			connection.query(SELECT_NONFICTION_QUERY,(err,results)=>{
+				if(err){
+					return res.send(err)
+				}
+				else
+				{
+					return res.json({
+						data:results
+					})
+				}
+			});
+			
+		});
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 		app.get('/',(req,res)=>{
 			res.send('go to /booklist to see booklist')
 			// res.json(booklist);
